2732d7a797590598a8961b877e86b752fc0f6b53
Shuvro Saha <sahashuvro74@gmail.com>
2026-08-07 01:33:48 +0600

    Create Experimental Login Logic for Credentials

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,14 @@
 import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import CredentialsProvider from "next-auth/providers/credentials"
+
+
+
+const userList = [
+    {name: "hablu", password:"1234"},
+    {name: "bablu", password:"8284"},
+    {name: "dablu", password:"3231"},
+]
+
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -14,9 +23,21 @@ export const authOptions = {
 
     credentials: {
       username: { label: "Username", type: "text", placeholder: "jsmith" },
-      password: { label: "Password", type: "password" }
+      password: { label: "Password", type: "password" },
+      secretCode: { label: "Secret Code", type: "number", placeholder:"enter code", },
     },
     async authorize(credentials, req) {
+      const {username, password, secretCode} = credentials;
+      
+      
+      const user = userList.find((u) => u.name == username);
+      if(!user) return null;
+
+      const isPasswordOk = user.password == password;
+
+      if(isPasswordOk){
+        return user;l
+      }
       
       // my own login logic  
       return null
@@ -25,4 +46,5 @@ export const authOptions = {
   ],
 }
 
-export default NextAuth(authOptions)
+const handler = NextAuth(authOptions)
+export { handler as GET, handler as POST }
---
+++ src/components/LoginButton.jsx
@@ -0,0 +1,10 @@
+'use client'
+import { signIn } from "next-auth/react"
+
+import React from 'react'
+
+const LoginButton = () => {
+  return <button className='btn' onClick={()=> signIn()}>Login Now</button>
+}
+
+export default LoginButton
